f5f8a2ce9b8db78a0bf9259c116d3f077ba85cc3
Muhammet Çelik <muhammetclk56@gmail.com>
2022-06-10 12:15:16 +0300

    BasePerson classi olusturuldu ve isim,soyisim,telefon numarasi tutan property tanimlamalari yapildi.Main metodunda BasePerson type`inda liste olusturuldu ve 5 kisinin numarasi varsayilan olarak eklendi.

--- TelefonRehberiUygulamasi/TelefonRehberiUygulamasi/Program.cs
+++ TelefonRehberiUygulamasi/TelefonRehberiUygulamasi/Program.cs
@@ -10,6 +10,35 @@ namespace TelefonRehberiUygulamasi
     {
         static void Main(string[] args)
         {
+            List<BasePerson> basePeople = new List<BasePerson> {//BasePerson tipinde isim,soyisim,numara tutan liste. 
+                new BasePerson() { FirstName = "Burak", LastName = "Yilmaz", TelNumber ="12345678900"},//ilk deger atamalari yapildi.
+                new BasePerson() { FirstName = "Emre", LastName = "Belozoglu", TelNumber ="12345678911"},
+                new BasePerson() { FirstName = "Hakan", LastName = "Calhanoglu", TelNumber ="12345678922"},
+                new BasePerson() { FirstName = "Mesut", LastName = "Ozil", TelNumber ="12345678933"},
+                new BasePerson() { FirstName = "Ferdi", LastName = "Kadioglu", TelNumber ="12345678944"}
+            };
+            Console.ReadLine();
+        }
+    }
+    class BasePerson//base clasimiz.Icinde isim,soyisim,numara propertylerini tutuyor.
+    {
+        string _FirstName { get; set; }
+        string _LastName { get; set; }
+        string _TelNumber { get; set; }
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+        public string TelNumber
+        {
+            get { return _TelNumber; }
+            set { _TelNumber = value; }
         }
     }
 }
