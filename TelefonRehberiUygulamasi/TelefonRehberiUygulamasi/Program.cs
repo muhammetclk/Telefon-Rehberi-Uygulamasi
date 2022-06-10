b791305d1c19c72434d96067d40e64d27cbfb3de
Muhammet Çelik <muhammetclk56@gmail.com>
2022-06-10 12:42:33 +0300

    Numara kaydetme islemi yapan metod yazildi.Numara silme islemi yazan metod yazildi.Girilen kisinin varliginin kontrol edildigi Control metodu yazildi.

--- TelefonRehberiUygulamasi/TelefonRehberiUygulamasi/Program.cs
+++ TelefonRehberiUygulamasi/TelefonRehberiUygulamasi/Program.cs
@@ -89,14 +89,93 @@ namespace TelefonRehberiUygulamasi
     }
     class Action : BasePerson//Islemleri yapacak olan metodlari tutan ve Base clasindaki propertyleri kullanabilmek icin miras alan classimiz.
     {
-        /* public int Control(List<BasePerson> basePeople)//isim,soyisim,numaraya gore arama yapan ve kisi bulunursa indexini donduren yoksa -1 donduren metodumuz.
-         { }*/
+        public int Control(List<BasePerson> basePeople)//isim,soyisim,numaraya gore arama yapan ve kisi bulunursa indexini donduren yoksa -1 donduren metodumuz.
+        {
+            Console.Write("FirstNameOrLastNameOrTelnumber:");
+            string NameOrSurnameOrTelnumber = Console.ReadLine();//aranacak kisi bilgisi.
+            bool IsThere = true;//aranacak kisinin varligini gosteren kontrol ifadesi.
+            int count = 0;//aranacak kisinin indexini dondurecek degisken.
+            foreach (var basePerson in basePeople)
+            {
+                if (NameOrSurnameOrTelnumber.ToLower() == basePerson.FirstName.ToLower() || NameOrSurnameOrTelnumber.ToLower() == basePerson.LastName.ToLower() || NameOrSurnameOrTelnumber.ToLower() == basePerson.TelNumber.ToLower())
+                {
+                    IsThere = true;//eger aranacak kisi eslesirse true olup donguyu bitirecek.
+                    break;
+                }
+                else
+                {
+                    IsThere = false;//eger aranan kisi eslesmediyse false olup listenin sonuna kadar kontrol  edilecek.
+                    count++;//eslesme yoksa degiskenin degerini arttiracak ve indexini bulmamizi saglayacak.
+                }
+            }
+            int Result;
+            Result = IsThere == true ? count : -1;//aranan bulunursa indexini, bulunmassa -1 dondurucek yapi. 
+            return Result;
+        }
 
         public void SaveNumber(List<BasePerson> basePeople)//yeni numara kaydetme islemi yapan metod.
         {
+            Console.Write("Please Enter FirstName:");
+            FirstName = Console.ReadLine();//isim atamasi.
+            Console.Write("Please Enter LastName:");
+            LastName = Console.ReadLine();//soyisim atamasi.
+            Console.Write("Please Enter Phone Number:");
+            TelNumber = Console.ReadLine();//numara atamasi.
+            basePeople.Add(new BasePerson { FirstName = FirstName, LastName = LastName, TelNumber = TelNumber });//yeni kisiyi listeye akleme islemi.
+            Console.WriteLine("Saved...");
+            Console.WriteLine("Returns to main menu");
         }
         public void DeleteNumber(List<BasePerson> basePeople)//numara silme islemi yapacak olan metod
         {
+            while (true)
+            {
+                Console.WriteLine("Please enter the name or last name or Telnumber of the person whose number you want to delete:");
+                int inComingValue = Control(basePeople);//silinecek ismin kontrolu yapilacak ve indexini getirecek Control metod cagrimi.
+                if (inComingValue != -1)//gelen deger -1 degilse kisi bulunmustur ve burasi calisir.
+                {
+                    Console.WriteLine("Do you approve " + basePeople[inComingValue].FirstName + " " + basePeople[inComingValue].LastName + " " + "to be deleted from the directory? (y / n)");
+                    Console.Write("Process:");//bulunan kisinin silinmesinin onay islemi.
+                    char Process = Convert.ToChar(Console.ReadLine());
+                    if (Process == 'y' || Process == 'Y')//eger y girilirse silinme islemi gerceklesir ve ana menuye doner.
+                    {
+                        basePeople.Remove(basePeople[inComingValue]);//kisi listeden silindi.
+                        Console.WriteLine("Deleted...");
+                        Console.WriteLine("Returns to main menu");
+                        break;
+                    }
+                    else if (Process == 'n' || Process == 'N')//eger n girilirse silme iptal olur ve ana menuye doner.
+                    {
+                        Console.WriteLine("Returns to main menu ");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong character..." + "\nReturns to main menu");//eger Y ve N disinda bir karakter girilirse karakter hatasi verir ve ana menuye donulur.
+                        break;
+                    }
+                }
+                else  //eger gelen deger -1 ise bu kisma girer.
+                {
+                    Console.WriteLine("The person you are  looking for was not found");//aranan kisini olmadigini soyler.
+                    Console.WriteLine("Please select an operation");//secim yapmamizi ister 1 icin silmeyi bitir,2 icin tekrar dene.
+                    Console.WriteLine("1-End Deletion...");
+                    Console.WriteLine("2-Try Again...");
+                    Console.Write("Process:");
+                    int Process = Convert.ToInt32(Console.ReadLine());
+                    if (Process == 1)//1 basilirsa ana menue doner.
+                    {
+                        Console.WriteLine("Returns to main menu");
+                        break;
+                    }
+                    else if (Process == 2)//2 basilirsa silme adimi tekrar calisir ve tekrar kisi bilgisi ister.
+                        continue;
+                    else//yanlis karakter girilmesi durumunda hata verir ve ana menuye doner.
+                    {
+                        Console.WriteLine("Wrong character..." + "\nReturns to main menu");
+                        break;
+                    }
+                }
+            }
         }
         public void UpdateNumber(List<BasePerson> basePeople)//numara guncelleme yapacak olan metod.
         {
